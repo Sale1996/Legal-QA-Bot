7c928514520b902f319290f8bc824d161f38afae
Sale1996 <ftnmejl96@gmail.com>
2020-02-01 14:22:11 +0100

    Crud deo za dodavanje sparql querija dodat na front end i povezan sa back-end-om

--- Pravna_semanticki_projekat/ngAdminApp/src/app/components/add-entity/sparql-questions/sparql-questions-modal/sparql-questions-modal.component.ts
+++ Pravna_semanticki_projekat/ngAdminApp/src/app/components/add-entity/sparql-questions/sparql-questions-modal/sparql-questions-modal.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { LegalEntity } from 'src/app/model/LegalEntity.model';
+import { SparqlQuestionService } from 'src/app/services/sparqlQuestion.service';
+import { SparqlQuestion } from 'src/app/model/SparqlQuestion.model';
+import { Observable } from 'rxjs/internal/Observable';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { LegalEntityService } from 'src/app/services/legalEntity.service';
+import { OntologyParameter } from 'src/app/model/OntologyParameter.model';
 
 @Component({
   selector: 'app-sparql-questions-modal',
@@ -7,9 +15,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SparqlQuestionsModalComponent implements OnInit {
 
-  constructor() { }
+
+  @Input() id?: number;
+  @Output() question: EventEmitter<any> = new EventEmitter();
+  submitBtnText: string;
+  questionForm: FormGroup;
+  entities$: Observable<LegalEntity[]>;
+  selectedEntity : LegalEntity;
+
+
+  constructor(public activeModal: NgbActiveModal, private formBuilder: FormBuilder, private entityService : LegalEntityService, private questionService : SparqlQuestionService) {}
 
   ngOnInit() {
+
+    this.getEntities();
+
+    this.questionForm = this.formBuilder.group({
+      sparqlQuestionId: [''],
+      queryText: ['', Validators.required],
+      sparqlQueryText: ['', Validators.required],
+      entityId: [''],
+      legalEntity: [],
+    });
+
+    if (this.id) {
+      this.submitBtnText = 'Save Changes';
+      this.getQuestionById(this.id);
+    } else {
+      this.submitBtnText = 'Add Question';
+    }
   }
 
+  getEntities() {
+    this.entities$ = this.entityService.getEntities();
+  }
+
+  getQuestionById(id: number) {
+    
+    this.questionService.getSparqlQuestionById(id).subscribe(
+      (question: SparqlQuestion) => {
+        this.questionForm.patchValue(question);
+        this.selectEntity(question.legalEntity.legalEntityId);
+        this.questionForm.patchValue({entityId: question.legalEntity.legalEntityId})
+
+      }
+    );
+    
+  }
+
+  selectEntity(entityId : number){
+    this.entityService.getEntityById(entityId).subscribe((data: LegalEntity) => this.selectedEntity = data );
+  }
+
+
+  onSubmit() {
+    
+    if (this.questionForm.valid) {
+      var return_value = this.questionForm.value as SparqlQuestion;
+      return_value.legalEntity = this.selectedEntity;
+      this.question.emit(return_value);
+      this.activeModal.close();
+    }
+    
+  }
 }
--- Pravna_semanticki_projekat/ngAdminApp/src/app/components/add-entity/sparql-questions/sparql-questions.component.ts
+++ Pravna_semanticki_projekat/ngAdminApp/src/app/components/add-entity/sparql-questions/sparql-questions.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SparqlQuestion } from 'src/app/model/SparqlQuestion.model';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { SparqlQuestionService } from 'src/app/services/sparqlQuestion.service';
+import { ConfirmationModalComponent } from 'src/app/_shared/confirmation-modal/confirmation-modal.component';
+import { LegalEntitiesModelComponent } from '../legal-entities/legal-entities-model/legal-entities-model.component';
+import { SparqlQuestionsModalComponent } from './sparql-questions-modal/sparql-questions-modal.component';
 
 @Component({
   selector: 'app-sparql-questions',
@@ -7,9 +14,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SparqlQuestionsComponent implements OnInit {
 
-  constructor() { }
+
+  questions$: Observable<SparqlQuestion[]>;
+
+  constructor(private modalService: NgbModal, private questionService : SparqlQuestionService) { }
 
   ngOnInit() {
+    this.getQuestions();
   }
 
+  getQuestions() {
+    this.questions$ = this.questionService.getQuestions();
+  }
+
+  deleteQuestion(question: SparqlQuestion) {
+    const deleteModalRef = this.modalService.open(ConfirmationModalComponent,
+      {
+        centered: true,
+        backdropClass: 'custom-modal-backdrop'
+      });
+    deleteModalRef.componentInstance.title = 'Delete question';
+    deleteModalRef.componentInstance.message = 'Are you sure you want to delete ' + '"' + question.queryText + ' " ?';
+    deleteModalRef.componentInstance.answer.subscribe(
+      (answer: boolean) => {
+        if (answer) {
+          this.questionService.deleteSparqlQuestion(question.sparqlQuestionId).subscribe(
+            () => {
+              this.getQuestions();
+            }
+          );
+        }
+      }
+    );
+  }
+
+  openQuestionModal(id?: number) {
+    const agentModalRef = this.modalService.open(SparqlQuestionsModalComponent,
+      {
+        centered: true,
+        backdropClass: 'custom-modal-backdrop'
+      });
+
+    if (id) {
+      agentModalRef.componentInstance.id = id;
+    }
+    agentModalRef.componentInstance.question.subscribe(
+      (question: SparqlQuestion) => {
+          if (question.sparqlQuestionId) {
+            this.questionService.updateSparqlQuestion(question).subscribe(
+              () => {
+                this.getQuestions();
+              }
+            );
+          } else {  
+            this.questionService.createSparqlQuestion(question).subscribe(
+                () => {
+                  this.getQuestions();
+                }
+              );
+              
+          }
+      }
+    );
+  }
 }
